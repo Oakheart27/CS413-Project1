22147031e90806d1d7a48230cbe10314808cf0b3
Oakheart27 <gytm337@gmail.com>
2019-09-24 10:21:48 -0700

    File folder added
    Moved project 1 into file folder for project 1

--- Project 1/game.js
+++ Project 1/game.js
@@ -1,17 +1,21 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x33cc33});
+var renderer = PIXI.autoDetectRenderer(500, 500, {backgroundColor: 0x33cc33});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
+
+var background = new PIXI.Sprite(PIXI.Texture.fromImage("background.png"));
+stage.addChild(background);
+var score = 0;
+var scoreoutput = new PIXI.Text("Fires destroyed: 0");
+stage.addChild(scoreoutput);
 
 var firefighter = new PIXI.Sprite(PIXI.Texture.fromImage("firefighter.png"));
 var fire = new PIXI.Sprite(PIXI.Texture.fromImage("fire.png"));
 var gameover = new PIXI.Sprite(PIXI.Texture.fromImage("gameOver.png"));
 
-var score = 0;
-
-firefighter.health = 100;
+//firefighter.health = 100;
 
 firefighter.position.x = 100;
 firefighter.position.y = 100;
@@ -46,7 +50,8 @@ function keydownEventHandler(e) {
   {
     fire.position.x = Math.floor(Math.random() * 300) + 50;
     fire.position.y = Math.floor(Math.random() * 300) + 50;
-    score ++;
+    score++;
+    scoreoutput.text = "Fires destroyed: " + score;
 
     if (score == 5)
     {
