926522511ed44c983994898f91566b4512ed48eb
Oakheart27 <gytm337@gmail.com>
2019-10-08 01:19:51 -0700

    Comments added to Project 2
    Commetns in game script
    Addition of runner sprite sheet

--- Project 2/game.js
+++ Project 2/game.js
@@ -3,17 +3,18 @@ var gameport = document.getElementById("gameport");
 var renderer = PIXI.autoDetectRenderer(1675, 500, {backgroundColor: 0x33cc33});
 gameport.appendChild(renderer.view);
 
+// creates containers 
 var stage = new PIXI.Container();
+var race = new PIXI.Container();
 
 // add background images
 var mainMenu = new PIXI.Sprite(PIXI.Texture.fromImage("MainMenu.png"));
 var raceTrack = new PIXI.Sprite(PIXI.Texture.fromImage("racetrack.png"));
 var credits = new PIXI.Sprite(PIXI.Texture.fromImage("credits.png"));
+var instructions = new PIXI.Sprite(PIXI.Texture.fromImage("howto.png"));
 
 // adds game elements
-var fire = new PIXI.Sprite(PIXI.Texture.fromImage("fire.png"));
-var gameover = new PIXI.Sprite(PIXI.Texture.fromImage("gameOver.png"));
-var instructions = new PIXI.Sprite(PIXI.Texture.fromImage("howto.png"));
+var firefighter = new PIXI.Sprite(PIXI.Texture.fromImage("firefighter.png"));
 
 // adds button elements
 var playBtn = new PIXI.Sprite(PIXI.Texture.fromImage("playBtn.png"));
@@ -22,8 +23,6 @@ var creditBtn = new PIXI.Sprite(PIXI.Texture.fromImage("creditsBtn.png"));
 var backBtn = new PIXI.Sprite(PIXI.Texture.fromImage("returnBtn.png"));
 
 // sets position of game elements
-fire.position.x = 50;
-fire.position.y = 50;
 playBtn.position.x = 25;
 playBtn.position.y = 425;
 instructionBtn.position.x = 200;
@@ -31,22 +30,27 @@ instructionBtn.position.y = 425;
 creditBtn.position.x = 375;
 creditBtn.position.y = 425;
 
-// adds main menu elements to the screen
+// adds main menu elements to the stage container
 stage.addChild(mainMenu);
-stage.addChild(fire);
 stage.addChild(playBtn);
 stage.addChild(instructionBtn);
 stage.addChild(creditBtn);
+
+// adds gameplay elements to race container
+race.addChild(raceTrack);
+race.addChild(firefighter);
 
 // Sets buttons to interactive
 playBtn.interactive = true;
 instructionBtn.interactive = true;
 creditBtn.interactive = true;
+backBtn.interactive = true;
 
 // assigns hander functions to button
 playBtn.on('mousedown', playHandler);
 instructionBtn.on('mousedown', instructionHandler);
 creditBtn.on('mousedown', creditsHandler);
+backBtn.on('mousedown', menuReturn);
 
 // starts game
 function playHandler(e)
@@ -58,7 +62,45 @@ function playHandler(e)
   stage.removeChild(creditBtn);
 
   // adds play elements
-  stage.addChild(raceTrack);
+  stage.addChild(race);
+
+  // Accesses sprite files
+  PIXI.loader
+    .add("assets.json")
+    .load(ready);
+}
+
+function ready()
+{
+
+  firefighter.position.y = 300; // sets firefighter position
+  firefighter.interactive = true; // makes object interactive
+  firefighter.on('mousedown', mouseHandler);
+
+  var standing = new PIXI.Sprite(PIXI.Texture.fromFrame("runner1.png"));
+  standing.scale.x = 1;
+  standing.scale.y = 1;
+  standing.position.x = 50;
+  standing.position.y = 200;
+  race.addChild(standing);
+
+  // creates array
+  var frames = [];
+
+  // adds images of runner to frame array
+  for (var i=1; i<=5; i++) {
+    frames.push(PIXI.Texture.fromFrame('runner' + i + '.png'));
+  }
+
+  // creates movie clip and runs through array
+  person = new PIXI.extras.MovieClip(frames);
+  person.scale.x = 1;
+  person.scale.y = 1;
+  person.position.x = 200;
+  person.position.y = 200;
+  person.animationSpeed = 0.1;
+  person.play();
+  race.addChild(person); // adds runner to race container
 }
 
 // displays how to play information
@@ -89,7 +131,36 @@ function creditsHandler(e)
   // displays credits for game development
   stage.addChild(credits);
   stage.addChild(backBtn);
+  backBtn.position.x = 375;
+  backBtn.position.y = 425;
 }
+
+// returns main menu elements to stage
+function menuReturn(e)
+{
+  stage.addChild(mainMenu);
+  stage.addChild(playBtn);
+  stage.addChild(instructionBtn);
+  stage.addChild(creditBtn);
+  stage.removeChild(credits);
+  stage.removeChild(instructions);
+  stage.removeChild(backBtn);
+}
+
+function mouseHandler(e) {
+  var new_x = firefighter.position.x +=10;
+  createjs.Tween.get(hero.position).to({x: new_x}, 1000, createjs.Ease.bounceOut);
+}
+
+function keydownEventHandler(e)
+{
+  if (e.keyCode == 13) // Enter key
+  {
+    standing.position.x += 10; // moves standing man horizontally across screen
+  }
+}
+
+document.addEventListener('keydown', keydownEventHandler);
 
 function animate() {
   requestAnimationFrame(animate);
